11edc88658399b4519d256b1afcb1753f800622d
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-09 10:28:48 +0900

    update json.py

--- biotp/json.py
+++ biotp/json.py
@@ -2,7 +2,7 @@ import json
 
 def output_acc_org_asm(filename):
     with open(filename, 'r') as handle:
-        for i, line in enumerate(handle, 0):
+        for i, line in enumerate(handle, 1):
             record = json.loads(line)
             acc = record['accession']
             org = record['organism']['organismName'].replace(' ', '_')
