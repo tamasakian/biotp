e05784ce21124c66b1bf1d9a9a01fc0e339ff7a7
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-01 09:45:52 +0900

    feat: add extract_protein_by_entry

--- biotp/__main__.py
+++ biotp/__main__.py
@@ -7,6 +7,7 @@ from biotp import gff
 from biotp import gff3
 from biotp import jcvi
 from biotp import json
+from biotp import pfam
 
 def main():
     args = parse_args()
@@ -19,46 +20,47 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('function', choices=functions)
-    parser.add_argument('args', type=str, nargs='*')
+    parser.add_argument("function", choices = functions)
+    parser.add_argument("args", type = str, nargs = "*")
     args = parser.parse_args()
     return args
 
 functions = {
-    'slice_rgo_by_hgt': blast.slice_rgo_by_hgt,
-    'detect_hgt': blast.detect_hgt, 
-    'slice_results_by_rgo': blast.slice_results_by_rgo,
-    'slice_results_by_rgo_2': blast.slice_results_by_rgo_2, 
-    'slice_hits_by_crossover_group': blast.slice_hits_by_crossover_group,
-    'output_besthit_for_subgroup': blast.output_besthit_for_subgroup,
-    'output_ids': fasta.output_ids,
-    'rename_header': fasta.rename_header,
-    'rename_headers_to_features': fasta.rename_headers_to_features,
-    'rename_headers_feature': fasta.rename_headers_feature,
-    'prefix_to_headers': fasta.prefix_to_headers,
-    'slice_records_by_names': fasta.slice_records_by_names, 
-    'slice_headers_by_ids': fasta.slice_headers_by_ids,
-    'slice_seq_flanking_region': fasta.slice_seq_flanking_region,
-    'slice_seq_upstream_region': fasta.slice_seq_upstream_region,
-    'split_multi_into_single': fasta.split_multi_into_single,
-    'merge_seqs_by_seqids': fasta.merge_seqs_by_seqids,
-    'remove_n_from_seqs': fasta.remove_n_from_seqs,
-    'generate_all_introns': fasta.generate_all_introns,
-    'slice_lines_by_seqids': gff.slice_lines_by_seqids,
-    'output_seqid_strand_locs_by_pepid': gff.output_seqid_strand_locs_by_pepid,
-    'generate_coordinate_all_introns': gff.generate_coordinate_all_introns,
-    "generate_introns": gff.generate_introns, 
-    'make_dict_pepid': gff.make_dict_pepid,
-    "output_seqid_strand_locs_by_gene_id": gff3.output_seqid_strand_locs_by_gene_id, 
-    "generate_upstream_regions": gff3.generate_upstream_regions, 
-    "generate_downstream_regions": gff3.generate_downstream_regions, 
-    'output_blocks': jcvi.output_blocks,
-    'output_longest_one_to_one_microsynteny': jcvi.output_longest_one_to_one_microsynteny,
-    'output_longest_one_to_two_microsynteny': jcvi.output_longest_one_to_two_microsynteny,
-    'output_longest_one_to_three_microsynteny': jcvi.output_longest_one_to_three_microsynteny,
-    'output_besthit_one_to_two_microsynteny': jcvi.output_besthit_one_to_two_microsynteny,
-    "output_besthit_one_to_two_synteny": jcvi.output_besthit_one_to_two_synteny, 
-    'output_acc_org_asm': json.output_acc_org_asm
+    "slice_rgo_by_hgt": blast.slice_rgo_by_hgt,
+    "detect_hgt": blast.detect_hgt,
+    "slice_results_by_rgo": blast.slice_results_by_rgo,
+    "slice_results_by_rgo_2": blast.slice_results_by_rgo_2,
+    "slice_hits_by_crossover_group": blast.slice_hits_by_crossover_group,
+    "output_besthit_for_subgroup": blast.output_besthit_for_subgroup,
+    "output_ids": fasta.output_ids,
+    "rename_header": fasta.rename_header,
+    "rename_headers_to_features": fasta.rename_headers_to_features,
+    "rename_headers_feature": fasta.rename_headers_feature,
+    "prefix_to_headers": fasta.prefix_to_headers,
+    "slice_records_by_names": fasta.slice_records_by_names,
+    "slice_headers_by_ids": fasta.slice_headers_by_ids,
+    "slice_seq_flanking_region": fasta.slice_seq_flanking_region,
+    "slice_seq_upstream_region": fasta.slice_seq_upstream_region,
+    "split_multi_into_single": fasta.split_multi_into_single,
+    "merge_seqs_by_seqids": fasta.merge_seqs_by_seqids,
+    "remove_n_from_seqs": fasta.remove_n_from_seqs,
+    "generate_all_introns": fasta.generate_all_introns,
+    "slice_lines_by_seqids": gff.slice_lines_by_seqids,
+    "output_seqid_strand_locs_by_pepid": gff.output_seqid_strand_locs_by_pepid,
+    "generate_coordinate_all_introns": gff.generate_coordinate_all_introns,
+    "generate_introns": gff.generate_introns,
+    "make_dict_pepid": gff.make_dict_pepid,
+    "output_seqid_strand_locs_by_gene_id": gff3.output_seqid_strand_locs_by_gene_id,
+    "generate_upstream_regions": gff3.generate_upstream_regions,
+    "generate_downstream_regions": gff3.generate_downstream_regions,
+    "output_blocks": jcvi.output_blocks,
+    "output_longest_one_to_one_microsynteny": jcvi.output_longest_one_to_one_microsynteny,
+    "output_longest_one_to_two_microsynteny": jcvi.output_longest_one_to_two_microsynteny,
+    "output_longest_one_to_three_microsynteny": jcvi.output_longest_one_to_three_microsynteny,
+    "output_besthit_one_to_two_microsynteny": jcvi.output_besthit_one_to_two_microsynteny,
+    "output_besthit_one_to_two_synteny": jcvi.output_besthit_one_to_two_synteny,
+    "output_acc_org_asm": json.output_acc_org_asm,
+    "extract_protein_by_entry": pfam.extract_protein_by_entry,
 }
 
 if __name__ == "__main__":
---
+++ biotp/pfam.py
@@ -0,0 +1,36 @@
+def extract_protein_by_entry(input_file: str, output_file: str, *entry: str) -> None:
+    """
+    Extract unique protein names corresponding to specific Pfam entries.
+
+    Args:
+        input_file:  Input filename.
+        output_file: Output filename.
+        entry:       Pfam entry to filter.
+
+    """
+    if not entry:
+        raise ValueError("[ERROR] No Pfam entry provided.")
+
+    # Initialize a dictionary to hold unique proteins for each entry
+    entry2protein = {e: set() for e in entry}
+
+    with open(input_file, "r") as input_handle:
+        for line in input_handle:
+            if line.startswith("#") or not line.strip():
+                continue
+
+            columns = line.split()
+
+            if len(columns) >= 4:
+                pfam = columns[1]
+                protein_name = columns[3]
+
+                if pfam in entry2protein:
+                    entry2protein[pfam].add(protein_name)
+
+    common_proteins = set.intersection(*entry2protein.values())
+
+    with open(output_file, "w") as output_handles:
+        output_handles.write("\n".join(sorted(common_proteins)))
+
+    print(f"[INFO] Extracted {len(common_proteins)} unique proteins for Pfam entries: {', '.join(entry)}.")
