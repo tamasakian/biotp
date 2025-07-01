022cce5ecec0956035c8d02e3b625f39957618cc
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-01 11:02:32 +0900

    upgrade: version 1.4.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="biotp",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(),
 
     author="tamasakian",
     description="A Bioinformatics Textfile Processor",
-    
+
     install_requires=[
-        "pandas", 
+        "pandas",
         "biopython",
     ],
 )
